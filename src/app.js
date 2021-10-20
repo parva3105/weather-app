7b9d73b66779f7b978cd83c5a362652dc874584c
Parva Shah <parvashah310501@gmail.com>
2021-10-21 02:14:24 +0530

    Final Setup

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+const port = process.env.PORT || 3000 //3000 to run locally
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -107,6 +108,6 @@ app.get('*',(req , res) => {
 //app.com/help
 //app.com/about
 
-app.listen(3000 ,() => {
-    console.log('Server is up on port 3000');
+app.listen(port ,() => {
+    console.log('Server is up on port '+ port);
 })
