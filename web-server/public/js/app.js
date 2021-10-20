ac15541ea6b07b7f760b3fcc248df28b59f0968f
Parva Shah <parvashah310501@gmail.com>
2021-10-20 21:14:20 +0530

    Completed, Ready to deploy

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,29 +1,26 @@
 console.log('Clien side js file loaded');
 
-fetch('http://localhost:3000/weather?address='+location+'').then((response) => {
-    response.json().then((data) => {
-        if(data.error) {
-            return console.log(data.error);
-        }
-        console.log(data.location);
-        console.log(data.forecast);
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1') 
+const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
     fetch('http://localhost:3000/weather?address='+location+'').then((response) => {
     response.json().then((data) => {
         if(data.error) {
-            return console.log(data.error);
+            messageOne.textContent = data.error
         }else {
-            console.log(data.location);
-            console.log(data.forecast);
+            messageOne.textContent = data.forecast 
+            messageTwo.textContent = data.location
+            // console.log(data.location);
+            // console.log(data.forecast);
             }
         })
     })
